53e92ff16b578ef8a92db8ecb2b3a4d07e01c2ad
shanjib <ishanjib@gmail.com>
2019-12-23 15:22:16 -0500

    #1 add code style

--- src/main/java/com/shanjib/finances/PersonalFinancesApp.java
+++ src/main/java/com/shanjib/finances/PersonalFinancesApp.java
@@ -7,12 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 @SpringBootApplication
 public class PersonalFinancesApp {
-    public static void main(final String[] args) {
-        log.info("test");
-        try {
-            SpringApplication.run(PersonalFinancesApp.class, args);
-        } catch (Exception e) {
-            log.error("Unable to start application context: ", e);
-        }
+
+  public static void main(final String[] args) {
+    log.info("test");
+    try {
+      SpringApplication.run(PersonalFinancesApp.class, args);
+    } catch (Exception e) {
+      log.error("Unable to start application context: ", e);
     }
+  }
 }
